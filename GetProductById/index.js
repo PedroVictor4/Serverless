d7f0c816e360a6467feb044c913b447b0ace87ef
PedroVictor4 <68966001+PedroVictor4@users.noreply.github.com>
2020-12-21 19:03:09 -0300

    Add files via upload

---
+++ GetProductById/index.js
@@ -0,0 +1,20 @@
+const {objectID, ObjectID} = require('mongodb')
+
+const createmMongoClient = require('../shared/mongoClient');
+
+
+
+module.exports = async function (context, req) {
+    const {id} = req.params;
+
+
+    const {client:  MongoClient,closeConnectionFn} = await 
+    createMongoClient();
+    const Products = MongoClient.collection('products');
+    const res = await Products.findOne({ _id: ObjectID(id)});
+    closeConnectionFn();
+    context.res ={
+        status:200,
+        body: res,
+    }
+    };
